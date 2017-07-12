21ab95199772b97041e30cb362f6f35ec563753a
Burcu Noyan <burcunoyan@gmail.com>
2017-07-11 21:46:58 -0400

    log incoming verb and route

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ var app = express();
 
 var port = 3000 || process.env.PORT;
 
+app.use(function(req, res, next) {
+  console.log(req.originalUrl);
+  console.log(req.method);
+  next();
+});
+
 app.get('/', function(req, res, next){
 	res.send('hello')
-})
+});
+
+app.use(function(req, res, next) {
+  res.send('you idiot');
+});
 
 app.listen(3000, function(){
 	console.log(`Listening intently on port ${port}`)
