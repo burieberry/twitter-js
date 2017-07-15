83a3c86dc77985c9e23e2ad038d55a0ed96a7c51
Burcu Noyan <burcunoyan@gmail.com>
2017-07-15 19:12:01 -0400

    create router instance; add router for '/' and '/users/:name'

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-// could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
-router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
-  console.log('tweets: ', tweets);
-  res.render( 'index', { tweets: tweets } );
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find({'name': name});
+  res.render('index', { tweets: list });
+});
 
+router.get('/', function(req, res) {
+  let tweets = tweetBank.list();
+  res.render('index', { tweets: tweets });
 });
 
 module.exports = router;
-
-
