0eddc7f0f4e815732158b1362e7a2c2664e94527
Burcu Noyan <burcunoyan@gmail.com>
2017-07-15 19:10:06 -0400

    require express, nunjucks, chalk, routes; use public route, get /, listen to port 3000

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-var express = require('express');
-var nunjucks = require('nunjucks');
-var routes = require('./routes')
-var app = express();
-
-var port = 3000 || process.env.PORT;
+const express = require('express');
+const app = express(); // store application instance in an app variable
+const port = 3000 || process.env.PORT;
+const chalk = require('chalk'); // color the console output
+const nunjucks = require('nunjucks'); // templating engine
+const routes = require('./routes');
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory for templates
 
-app.use(function(req, res, next) {
-  console.log(req.originalUrl);
-  console.log(req.method);
+app.use(express.static('public'));
+app.use('/', routes);
+
+// Log requests, url, status code
+app.use('/', function(req, res, next) {
+  console.log(chalk.blue(req.method, req.originalUrl));
+  res.on('finish', function() {
+     // console log the returned status code
+    return console.log(chalk.cyan(res.statusCode));
+  });
   next();
 });
 
-app.use(express.static('public'))
-
-app.get('/', routes);
-
+// Fallback for all page requests
 app.use(function(req, res, next) {
-  res.send('you idiot');
+  res.send('You idiot. This page does not exist.');
 });
 
-app.listen(3000, function(){
-	console.log(`Listening intently on port ${port}`)
-})
-
+// listen for requests on port 3000
+app.listen(port, function() {
+  console.log(chalk.magenta(`Listening intently on port ${port}`));
+});
