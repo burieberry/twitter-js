11c4506afe3bd04b7a1a84f0d26a0f2441bd1bc0
Burcu Noyan <burcunoyan@gmail.com>
2017-07-25 20:09:15 -0400

    install and use path for static directory

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express(); // store application instance in an app variable
-const port = 3000 || process.env.PORT;
+const path = require('path');
 const chalk = require('chalk'); // color the console output
 const nunjucks = require('nunjucks'); // templating engine
 const routes = require('./routes');
+const port = 3000 || process.env.PORT;
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // use nunjucks to give html files to res.render
 nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory
 
-app.use(express.static('public'));
+app.use('/', express.static(path.join(__dirname, '/public')));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
@@ -21,18 +22,17 @@ app.use(bodyParser.json());
 // route to homepage
 app.use('/', routes);
 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-});
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// });
 
 // Log requests, url, status code
 app.use('/', function(req, res, next) {
-  console.log(chalk.blue(req.method, req.originalUrl));
   res.on('finish', function() {
      // console log the returned status code
-    return console.log(chalk.cyan(res.statusCode));
+    console.log(chalk.blue(req.method), req.url, chalk.red(res.statusCode));
   });
   next();
 });
@@ -41,6 +41,7 @@ app.use('/', function(req, res, next) {
 app.use(function(req, res, next) {
   res.send('You idiot. This page does not exist.');
 });
+
 
 // listen for requests on port 3000
 app.listen(port, function() {
