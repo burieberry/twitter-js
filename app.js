28f9168a2924ec6700dce7b022fef80aa127ddca
Burcu Noyan <burcunoyan@gmail.com>
2017-07-29 17:30:29 -0400

    minor adjustments

--- app.js
+++ app.js
@@ -1,49 +1,43 @@
+'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express(); // store application instance in an app variable
 const path = require('path');
 const chalk = require('chalk'); // color the console output
-const nunjucks = require('nunjucks'); // templating engine
 const routes = require('./routes');
-const port = 3000 || process.env.PORT;
+const nunjucks = require('nunjucks'); // templating engine
 
+const app = express(); // store application instance in an app variable
+
+// Nunjucks templating setup
 app.set('view engine', 'html'); // have res.render work with html files
-app.engine('html', nunjucks.render); // use nunjucks to give html files to res.render
-nunjucks.configure('views', { noCache: true }); // point nunjucks to the proper directory
+app.engine('html', nunjucks.render); // give html files to res.render
+nunjucks.configure('views', { noCache: true }); // point nunjucks to views dir
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
-
-// parse application/json
-app.use(bodyParser.json());
-
-// route to homepage
-app.use('/', routes);
-
-// app.use(function (req, res) {
-//   res.setHeader('Content-Type', 'text/plain')
-//   res.write('you posted:\n')
-//   res.end(JSON.stringify(req.body, null, 2))
-// });
+// Body parsing middleware
+app.use(bodyParser.urlencoded({ extended: false })); // for HTML form submits
+app.use(bodyParser.json()); // for AJAX requests
 
 // Log requests, url, status code
-app.use('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.on('finish', function() {
-     // console log the returned status code
-    console.log(chalk.blue(req.method), req.url, chalk.red(res.statusCode));
+    console.log(chalk.blue(req.method), chalk.gray(req.url), chalk.yellow(res.statusCode));
   });
   next();
 });
 
+// Route to homepage
+app.use('/', routes);
+
 // Fallback for all page requests
 app.use(function(req, res, next) {
-  res.send('You idiot. This page does not exist.');
+  res.status(404).send('You idiot. This page does not exist.');
 });
 
+// Start the server
+const port = 3000 || process.env.PORT;
 
-// listen for requests on port 3000
 app.listen(port, function() {
   console.log(chalk.magenta(`Listening intently on port ${port}`));
 });
